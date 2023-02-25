6eedb52082f874be39cb7792a9228e142a00e0e2
Optimum <126137872+OptimumAI@users.noreply.github.com>
2023-02-25 07:03:28 +0530

    updated font size of the title

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
     
     <header>
       <video src="./video.mp4" loop autoPlay muted></video>
-      <h1 id = "title">Optimum AI <br></br> Assess and Improve Your Soft Skills</h1>
+      <h1 id = "title">style="font-size:10vw">Optimum AI <br></br> Assess and Improve Your Soft Skills</h1>
       <h2 id = "tagline">Take your Soft skills to the next level with our comprehensive assessment and personalized analysis.</h2>
       <div className="row">
         <button className="btn" style={{ cursor: "pointer" }}>
